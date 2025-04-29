41befdd22d64d445f0be16a680bd1bb1ad703d8b
MCsomm <109383329+MCsomm@users.noreply.github.com>
2025-04-30 01:01:48 +0200

    Fix error translation

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-export const MobileMenu = ({menuOpen, setMenuOpen}) => {
-    const { t } = useTranslation();
+export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        setMenuOpen(false); // Close the menu after changing language
+    };
 
     return (
         <div className={`fixed top-0 left-0 w-full bg-[rgba(10,10,10,0.8)] z-40 flex flex-col items-center justify-center
@@ -35,7 +40,7 @@ export const MobileMenu = ({menuOpen, setMenuOpen}) => {
                 {t('mobileMenu.about')}
             </a>
             <a
-                href="#Projects"
+                href="#projects"
                 onClick={() => setMenuOpen(false)}
                 className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
                             ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
@@ -50,6 +55,31 @@ export const MobileMenu = ({menuOpen, setMenuOpen}) => {
             >
                 {t('mobileMenu.contact')}
             </a>
+
+            {/* Language Switcher Buttons for Mobile */}
+            <div className="flex items-center space-x-4 mt-8">
+                <button
+                    onClick={() => changeLanguage('en')}
+                    className={`text-lg font-semibold text-gray-300 hover:text-white transition-colors
+                                ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
+                >
+                    ENG
+                </button>
+                <button
+                    onClick={() => changeLanguage('sk')}
+                    className={`text-lg font-semibold text-gray-300 hover:text-white transition-colors
+                                ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
+                >
+                    SK
+                </button>
+                <button
+                    onClick={() => changeLanguage('hu')}
+                    className={`text-lg font-semibold text-gray-300 hover:text-white transition-colors
+                                ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
+                >
+                    HUN
+                </button>
+            </div>
         </div>
     );
 };
